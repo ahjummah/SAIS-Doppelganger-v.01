1db61e02a1261d780c3afe93a3b93da8d7da3b85
faielomente <frlomente@up.edu.ph>
2016-05-28 16:15:20 +0800

    class-based create student

--- SAIS/WebApp/urls.py
+++ SAIS/WebApp/urls.py
@@ -5,6 +5,5 @@ from . import views
 urlpatterns = [
 	url(r'^$', IndexView.as_view(), name='index'),
 	url(r'^register$', RegistrationView.as_view(), name = 'register'),
-	url(r'^register/submit$', views.formRegister, name='submitForm'),
 	url(r'^login$', LoginView.as_view(), name = 'login'),
 ]
--- SAIS/WebApp/views.py
+++ SAIS/WebApp/views.py
@@ -18,25 +18,21 @@ class RegistrationView(View):
 	def get(self, request):
 		return render(request, 'register.html')
 
-def formRegister(request):
-	
-	student = Student()
-	studentInfo = SchoolInfo()
-	student.fname = request.POST['fname']
-	student.mname = request.POST['mname']
-	student.lname = request.POST['lname']
-	student.student_id = request.POST['student_id']
-	student.address = request.POST['address']
-	student.gender = request.POST['gender']
-	student.maritalstatus = request.POST['maritalstatus']
-	student.save()
-	studentInfo.student_id = student
-	studentInfo.course = request.POST['course']
-	studentInfo.year = request.POST['year']
-	studentInfo.sts_code = request.POST['sts_code']
-	studentInfo.save()
+	def post(self, request):
+		student = Student()
+		studentInfo = SchoolInfo()
+		student.fname = request.POST['fname']
+		student.mname = request.POST['mname']
+		student.lname = request.POST['lname']
+		student.student_id = request.POST['student_id']
+		student.address = request.POST['address']
+		student.gender = request.POST['gender']
+		student.maritalstatus = request.POST['maritalstatus']
+		student.save()
+		studentInfo.student_id = student
+		studentInfo.course = request.POST['course']
+		studentInfo.year = request.POST['year']
+		studentInfo.sts_code = request.POST['sts_code']
+		studentInfo.save()
 
-
-	return render(request, 'main.html')
-
-# Create your views here.
+		return render(request, 'main.html')
