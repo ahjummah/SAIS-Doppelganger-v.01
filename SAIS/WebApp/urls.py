dd12e9b8b94820a9931e4e26e3a5e5b135483100
faielomente <frlomente@up.edu.ph>
2016-05-28 18:59:12 +0800

    updated master

--- SAIS/WebApp/urls.py
+++ SAIS/WebApp/urls.py
@@ -3,17 +3,8 @@ from WebApp.views import *
 from . import views
 
 urlpatterns = [
-# <<<<<<< HEAD
 	url(r'^$', IndexView.as_view(), name='index'),
 	url(r'^register$', RegistrationView.as_view(), name = 'register'),
 	url(r'^login$', LoginView.as_view(), name = 'login'),
-# =======
-	# url(r'^$', views.index, name='index'),
-	# url(r'^register$', views.register, name = 'register'),
-	# url(r'^register/submit$', views.formRegister, name='submitRegister'),
-	# url(r'^login$', views.login, name = 'login'),
-	# url(r'^login/submit$', views.formLogin, name = 'submitLogin'),
 	url(r'^index-S$', StudentView.as_view(), name = 'viewIndex-S'),
-
-# >>>>>>> 0d62c8e8428b6a14d98dd428c8a81f0b8fa8a7f9
 ]
--- SAIS/WebApp/views.py
+++ SAIS/WebApp/views.py
@@ -20,12 +20,6 @@ class StudentView(View):
 	def get(self, request):
 		return render(self.equest,'indexStudent.html')
 
-# def indexStudent(request):
-# 	return render(request,'indexStudent.html')
-
-# def login(request):
-# 	return render(request, 'login.html')
-
 class LoginView(View):
 	"""docstring for LoginView"""
 	def get(self, request):
@@ -57,26 +51,6 @@ class LoginView(View):
 		else:  # Return an 'invalid login' error message.
 			print("The username and password were incorrect.")
 			return render(self.request, 'login.html')
-
-
-
-	# def formLogin(request):
-	# 	user = User()
-	# 	username = request.POST.get('student_id', False)
-	# 	password = request.POST.get('password', False)
-	# 	user = authenticate(username = username, password =password)
-	# 	if user is not None:
-	# 	    # the password verified for the user
-		
-	# 	  		login(request, user)
-	# 	  		return render(self.request, 'indexStudent.html')
-		 
-	# 	else:  # Return an 'invalid login' error message.
-	# 		print("The username and password were incorrect.")
-	# 		return render(self.request, 'login.html')
-
-# def register(request):
-# 	return render(request, 'register.html')
 
 class RegistrationView(View):
 	
@@ -110,30 +84,3 @@ class RegistrationView(View):
 		user.save()
 
 		return render(self.request, 'main.html')
-
-	# student = Student()
-	# studentInfo = SchoolInfo()
-	# user = User()
-
-	# student.fname = request.POST['fname']
-	# student.mname = request.POST['mname']
-	# student.lname = request.POST['lname']
-	# student.student_id = request.POST['student_id']
-	# student.address = request.POST['address']
-	# student.gender = request.POST['gender']
-	# student.maritalstatus = request.POST['maritalstatus']
-	# student.email = request.POST['email']
-	# student.save()
-
-	# studentInfo.student_id = student
-	# studentInfo.course = request.POST['course']
-	# studentInfo.year = request.POST['year']
-	# studentInfo.sts_code = request.POST['sts_code']
-	# studentInfo.save()
-
-	# user = User.objects.create_user(student.student_id, request.POST['email'], request.POST['password'])
-	# user.last_name = student.lname
-	# user.first_name = student.fname
-	# user.save()
-
-# Create your views here.
